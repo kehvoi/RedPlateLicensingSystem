d28621ab14413612b63dc9f55c52563c1ec8aff1
Chevannese <158519006+Chevannese@users.noreply.github.com>
2024-11-16 19:21:39 -0500

    Update Person.java
    Made Person an abstract class

--- RedPlateLicensingSystem/src/SystemOne/Person.java
+++ RedPlateLicensingSystem/src/SystemOne/Person.java
@@ -1,10 +1,14 @@
 package SystemOne;
-
-abstract class Person{
+public abstract class Person 
+{
 	protected Name fullName;
 	protected Date dateOfBirth;
 	protected int trn;
 	protected Address curAddr;
 	protected String emailAddr;
 	protected String contactNum;
+	
+	public abstract void Info();
+	
+
 }
