8d8c1ffba86d41a813dd04ad808b34482e8fbd79
Chevannese <158519006+Chevannese@users.noreply.github.com>
2024-11-25 19:29:00 -0500

    Update Driver.java
    turned Driver to a child class that inherits from user

--- RedPlateLicensingSystem/src/SystemOne/Driver.java
+++ RedPlateLicensingSystem/src/SystemOne/Driver.java
@@ -1,10 +1,34 @@
 package SystemOne;
-public class Driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+import java.time.LocalDate;
 
+public class Driver extends User
+{
+	
+	
+	//Primary Constructor
+	public Driver(Name name, LocalDate dob, int trn, Address addr, String email, String contactNum, String gender)
+	{
+		super(name, dob, trn,addr,email,contactNum,gender);
+	}
+	
+	//Copy Constructor
+	public Driver(Driver dri)
+	{
+		super(dri);
+	
+	}
+	 
+	public void Info()
+	{
+		System.out.println("The name of the driver is :" + name.getFirstName() + " " + name.getLastName());
+		System.out.println("The date of birth of the driver :" + dob);
+		System.out.println("The trn of driver is :" + trn);
+		System.out.println("The address of driver is :"+ addr);
+		System.out.println("The email of driver is  :" + email);
+		System.out.println("The contact number of driver is :"+ contactNum);
+		System.out.println("The gender of driver is :" + gender);
 	}
 
+	
 }
