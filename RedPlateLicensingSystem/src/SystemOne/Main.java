fe88c56bb9fb3d633151def3c6fac6e40d3d5bcb
Chevannese <158519006+Chevannese@users.noreply.github.com>
2024-11-28 16:28:18 -0500

    JCF Officer add ticket fully implemented

--- RedPlateLicensingSystem/src/SystemOne/Main.java
+++ RedPlateLicensingSystem/src/SystemOne/Main.java
@@ -1,4 +1,4 @@
-package System;
+package SystemOne;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -26,43 +26,47 @@ public class Main {
     public static String ppvLicense;
     public static int option;
     public static int license;
-    public static int offenseCodeInput;
-    public static int fineAmtInput;
+    
     public static int trnInput;
-    public static boolean ticketPayStatusInput = false;
+    public static String result;
+    //Global variables for ticket
+    
     public static LocalDate today = LocalDate.now();
     public static LocalDate weeks = today.plusWeeks(3);
+    public static LocalDate courtDateInput = weeks.plusWeeks(1);
+    public static int offenseCodeInput;
+    public static String offenseDescInput;
+    public static int fineAmtInput;
+    public static int totalUnpaidTicInput;
+    public static int totalFineAmtInput;
+    public static boolean warrantInput = false;
+    public static String ticketPayStatusInput = "Unpaid";
 
     public static void main(String[] args) 
     {
-    	//Declaration of ArrayList that contains Default Users
+    	//Declaration of ArrayLists 
     	ArrayList<Driver> Drivers = new ArrayList<>();
-
+    	ArrayList<Ticket> Tickets = new ArrayList<>();
+    	
 		//Default User 1
 		Name d1 = new Name("John", "Brown");
-		Address a1 = new Address(19,"Maldave Avenue","Bellford","Kingston");
+		Address a1 = new Address(19,"Maldave Avenue","Kingston");
 		Driver driver1 = new Driver(123456789,d1, LocalDate.parse("2000-05-05"),a1, "john.brown@gmail.com", "8768403526", "Male");
 
 		//Default User 2
 		Name d2 = new Name("Mary", "Anderson");
-		Address a2 = new Address(12, "Redwood Lane", "Sunnydale", "St.Andrew");
+		Address a2 = new Address(12, "Redwood Lane", "St.Andrew");
 		Driver driver2 = new Driver(987654321, d2, LocalDate.parse("1995-08-15"),a2, "mary.anderson@gmail.com", "8761234567", "Female");
 
 		//Default User 3
 		Name d3 = new Name("Sashana","Blackwood");
-		Address a3 = new Address(88,"Stockforth Road","BellRoad","St.Thomas");
+		Address a3 = new Address(88,"Stockforth Road","St.Thomas");
 		Driver driver3 = new Driver(100000000,d3, LocalDate.parse("2001-08-05"),a3, "sashana.blackwood@gmail.com", "8767252731", "Female");
 
 		Drivers.add(driver1);
 		Drivers.add(driver2);
 		Drivers.add(driver3);
-    	
 		
-		//ArrayList<Ticket> Tickets = new ArrayList<>();
-		
-    	
-    	
-    	
     	String filePath2 = "driver_records.csv";
         String filePath = "ticketCount.txt";
 
@@ -114,22 +118,44 @@ public class Main {
             				trnInput = scan.nextInt();
             				if (String.valueOf(trnInput).length() == 9)
             				{
-            					String result = searchDriverByTrn(filePath2, trnInput);
+            					result = searchDriverByTrn(filePath2, trnInput);
                 				if (result != null) {
                 		            System.out.println("Found driver record");
-                		            System.out.println("Please enter offense Code to apply to offender:");
+                		            System.out.println("Please enter offense Code (1-7) to apply to offender:"
+                		            		+ "\n1 - Aid And Abet No Driver’s Licence or Permit"
+                		            		+ "\n2 - Aid And Abet Operating Motor Vehicle..."
+                		            		+ "\n3 - Body Protruding "
+                		            		+ "\n4 - Breach of special permit"
+                		            		+ "\n5 - Careless Driving Causing Collision"
+                		            		+ "\n6 - Careless Driving Where No Collision Occurs"
+                		            		+ "\n7 - Carrying dangerous goods without the transport....");
                 		            offenseCodeInput = scan.nextInt();
                 		            while(offenseCodeInput <=0  || offenseCodeInput > 7)
                 		            {
-                		            		System.out.println("That offenseCode is not available. Please enter another one.");
+                		            		System.out.println("That offenseCode is not available. Please enter offense Code (1-7).");
                 		            		offenseCodeInput = scan.nextInt();
                 		            }
-                		            System.out.println("Successful input. I will add tickets in the next update! :)");
+                		            offenseDescInput = GenerateOffenseDesc(offenseCodeInput,offenseDescInput);
+                		            fineAmtInput = GenerateFine(offenseCodeInput, fineAmtInput);
+                		            Address c1 = new Address(144,"Maxfield Ave","Kingston");
                 		            
-                		            ArrayList<Ticket> NewTicket = new ArrayList<>();
+                		            Ticket tic = new Ticket(trnInput, ticketNum, today, weeks, offenseCodeInput, offenseDescInput, 
+                                            ticketPayStatusInput, fineAmtInput, courtDateInput, c1, totalUnpaidTicInput, 
+                                            totalFineAmtInput, warrantInput);
+                		            JCFOfficer jcf = new JCFOfficer();
+                		            tic.DisplayTicket();
+                		            jcf.DisplayTicketOfficer();
+                		            System.out.println("Driver details : " + result);
                 		            
-                		            //Ticket tic = new Ticket(trnInput,ticketNum, today,weeks,offenseCodeInput, offenseDesc, ticketPayStatusInput, fineAmt, totalUnpaidTic, totalFine);
-                		            //NewTicket.add(tic);
+                		            
+                		            Tickets.add(tic);
+                		            
+                		            AddTicket("ticket_records.csv", Tickets);
+                		            
+                		            ticketNum++;
+                		          //Saves the current ticket number to file
+                		            SaveTicketNum(filePath, ticketNum);
+                		            
                 		            
                 		            
                 		  
@@ -149,7 +175,7 @@ public class Main {
             				
             				System.out.println("");
             				
-            				ticketNum++;
+            				
             				
             			}
             		}
@@ -185,8 +211,8 @@ public class Main {
             }
             scan.close();
             
-            //Saves the current ticket number to file
-            SaveTicketNum(filePath, ticketNum);
+            
+            
             System.out.println("Thank you for your business. Have a nice day");
             
             
@@ -203,7 +229,7 @@ public class Main {
     //Methods for Drivers
     
   //Method to Load Default Driver Records
-    public static void DefaultDrivers(String filePath, ArrayList<Driver> parameterList) {
+    private static void DefaultDrivers(String filePath, ArrayList<Driver> parameterList) {
         File file = new File(filePath);
 
         // If the file doesn't exist, create it and add default drivers
@@ -239,7 +265,7 @@ public class Main {
     
     //Search TRN Method
     
-    public static String searchDriverByTrn(String filePath, int searchTrn) {
+    private static String searchDriverByTrn(String filePath, int searchTrn) {
         Map<Integer, String> index = new HashMap<>();
         
         // Read the file and build the index
@@ -271,7 +297,7 @@ public class Main {
     //Methods for Ticket Class
     
     // Method to load the ticket number from the file
-public static int LoadTicketNum(String filepath) {
+private static int LoadTicketNum(String filepath) {
         // Create a File object
         File file = new File(filepath);
 
@@ -305,7 +331,7 @@ public static int LoadTicketNum(String filepath) {
     }
 
     // Method to save the ticket number back to the file
-    public static void SaveTicketNum(String filepath, int ticketNum) {
+    private static void SaveTicketNum(String filepath, int ticketNum) {
         try (FileWriter writer = new FileWriter(filepath)) {
             // Write the ticket number to the file
             writer.write(String.valueOf(ticketNum));
@@ -315,21 +341,99 @@ public static int LoadTicketNum(String filepath) {
         }
     }
     
-    //Add Ticket Method
+    private static void AddTicket(String filePath3, ArrayList<Ticket> parameterList) {
+        File file = new File(filePath3);
+
+        // If the file doesn't exist, create it and add tickets
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+                System.out.println("Ticket is Successfully Loaded");
+                
+               // Write the drivers to the file
+            try (FileWriter writer = new FileWriter(file, true)) {
+                for (Ticket ticket : parameterList) {
+                    writer.write(ticket.toCSV());  // Get the CSV string and write it to the file
+                    writer.append("\n");  // Append a newline after each driver's record
+                }
+              } //end second try
+              catch (IOException e) {
+                System.out.println("An error occurred while saving the driver info.");
+                e.printStackTrace();
+              }//end catch  
+                
+                
+                
+            }//endif
+            else
+            {
+            	try (FileWriter writer = new FileWriter(file, true)) {
+                    for (Ticket ticket : parameterList) {
+                        writer.write(ticket.toCSV());  // Get the CSV string and write it to the file
+                        writer.append("\n");  // Append a newline after each driver's record
+                    }
+                  } //end second try
+                  catch (IOException e) {
+                    System.out.println("An error occurred while saving the driver info.");
+                    e.printStackTrace();
+                  }//end catch  
+            }
+
+           
+        }//end first try 
+        catch (IOException e) {
+            System.out.println("An error occurred while creating the file.");
+            e.printStackTrace();
+        }
+    }// end DefaultDrivers method
+    
+    //Generate Offense Description based on offense code
+    private static String GenerateOffenseDesc(int offenseCode, String offenseDesc)
+	{
+		switch (offenseCode)
+		{
+		case 1:
+			return offenseDesc = "Aid And Abet No Driver’s Licence or Permit";
+		case 2:
+			return offenseDesc = "Aid And Abet Operating Motor Vehicle...";
+		case 3:
+			return offenseDesc = "Body Protruding";
+		case 4:
+			return offenseDesc = "Breach of special permit";
+		case 5:
+			return offenseDesc = "Careless Driving Causing Collision";
+		case 6:
+			return offenseDesc = "Careless Driving Where No Collision Occurs";
+		case 7:
+			return offenseDesc = "Carrying dangerous goods without the transport....";
+		}
+		return offenseDesc = "";
+	}
     
     
+    //Generate Fine based on offenseCode
+    private static int GenerateFine (int offenseCode, int fineAmt)
+	{
+		switch (offenseCode)
+		{
+		case 1:
+			return fineAmt = 15000;
+		case 2:
+			return fineAmt = 30000;
+		case 3:
+			return fineAmt = 5000;
+		case 4:
+			return fineAmt = 30000;
+		case 5:
+			return fineAmt = 25000;
+		case 6:
+			return fineAmt = 11000;
+		case 7:
+			return fineAmt = 175000;
+		}
+		return fineAmt = 0;
+	}
     
-    
-   
-    
-    
-    
+    //Add Ticket Method 
     
 }//end of Class Main
-
-
-
-
-
-
-
