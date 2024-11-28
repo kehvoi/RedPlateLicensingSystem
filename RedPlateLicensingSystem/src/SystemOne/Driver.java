0513820b745e15251572e5841106305f1dc10c27
Chevannese <158519006+Chevannese@users.noreply.github.com>
2024-11-28 03:07:50 -0500

    Switched attribute trn to front here as well

--- RedPlateLicensingSystem/src/SystemOne/Driver.java
+++ RedPlateLicensingSystem/src/SystemOne/Driver.java
@@ -7,9 +7,9 @@ public class Driver extends User
 	
 	
 	//Primary Constructor
-	public Driver(Name name, LocalDate dob, int trn, Address addr, String email, String contactNum, String gender)
+	public Driver(int trn,Name name, LocalDate dob,  Address addr, String email, String contactNum, String gender)
 	{
-		super(name, dob, trn,addr,email,contactNum,gender);
+		super(trn,name, dob, addr,email,contactNum,gender);
 	}
 	
 	//Copy Constructor
@@ -21,18 +21,19 @@ public class Driver extends User
 	 
 	public void Info()
 	{
+		System.out.println("The trn of driver is :" + trn);
 		System.out.println("The name of the driver is :" + name.getFirstName() + " " + name.getLastName());
 		System.out.println("The date of birth of the driver :" + dob);
-		System.out.println("The trn of driver is :" + trn);
+		
 		System.out.println("The address of driver is :"+ addr);
 		System.out.println("The email of driver is  :" + email);
 		System.out.println("The contact number of driver is :"+ contactNum);
 		System.out.println("The gender of driver is :" + gender);
 	}
-
-	public String toCSV() 
+	
+	 public String toCSV() 
 	 {
-	        return name.getFirstName() + "," + name.getLastName() + "," + dob + "," + trn + "," +
+	        return trn + "," + name.getFirstName() + "," + name.getLastName() + "," + dob + "," +
 	               addr.getStreetNum() + "," + addr.getStreetName() + "," + addr.getCommunity() + "," +
 	               addr.getParish() + "," + contactNum + "," + gender;
 	 }
